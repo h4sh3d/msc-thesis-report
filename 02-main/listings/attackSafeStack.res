08f663e9d1e017d6e0210a1d7b52ea2455cebf4f
Joel Gugger <joel.gugger@rdlp.ch>
2017-09-22 14:59:04 +0200

    clean up and switch ot english

--- 02-main/listings/attackSafeStack.res
+++
@@ -1,2 +0,0 @@
-root@5017cf36ad41:/shared/rop# ./exploit2-ss.py
-root@5017cf36ad41:/shared/rop#
--- 02-main/listings/sample.c
+++
@@ -1,9 +0,0 @@
-unsigned add1(unsigned a, unsigned b) {
-  return a+b;
-}
-
-// Perhaps not the most efficient way to add two numbers.
-unsigned add2(unsigned a, unsigned b) {
-  if (a == 0) return b;
-  return add2(a-1, b+1);
-}
